80b84059161e8e94eb94efd9d72e899ed7f3bc39
Kamal Albousafi <kamal.albousafi@jacks.sdstate.edu>
2026-05-27 00:42:00 -0500

    Created a testset loader to handle the aa model during inference time for evaluate.py file
    #### Main changes
    * Adopted the code from datasets/loader.py to create a loader that suits the needs of the aa model for evaluate.py

--- datasets/test_loader.py
+++ datasets/test_loader.py
@@ -1,3 +1,6 @@
+# KRA Created:
+# - a simple change to loader.py so that the logic can work for the test set in evaluate.py
+
 import torch
 from torch_geometric.data import Dataset
 
@@ -27,7 +30,8 @@ class CombineDatasets(Dataset):
         self.dataset1.add_complexes(new_complex_list)
 
 
-def construct_loader(args, t_to_sigma, device):
+def construct_test_loader(args, t_to_sigma, device, test_split_path, limit_complexes):
+    args.limit_complexes = limit_complexes
     val_dataset2 = None
     transform = NoiseTransform(t_to_sigma=t_to_sigma, no_torsion=args.no_torsion,
                                all_atom=args.all_atoms, alpha=args.sampling_alpha, beta=args.sampling_beta,
@@ -82,13 +86,13 @@ def construct_loader(args, t_to_sigma, device):
                        'vdw_vol': args.vdw_vol}
         # KRA updated above to include vdw flags
         if args.dataset == 'pdbbind' or args.dataset == 'generalisation' or args.combined_training:
-            train_dataset = PDBBind(cache_path=args.cache_path, split_path=args.split_train, keep_original=True,
+            train_dataset = PDBBind(cache_path=args.cache_path, split_path=test_split_path, keep_original=True, require_ligand=True,
                                     num_conformers=args.num_conformers, root=args.pdbbind_dir,
                                     esm_embeddings_path=args.pdbbind_esm_embeddings_path,
                                     protein_file=args.protein_file, **common_args)
 
         if args.dataset == 'moad' or args.combined_training:
-            train_dataset2 = MOAD(cache_path=args.cache_path, split='train', keep_original=True,
+            train_dataset2 = MOAD(cache_path=args.cache_path, split='test', keep_original=True, require_ligand=True,
                                   num_conformers=args.num_conformers, max_receptor_size=args.max_receptor_size,
                                   remove_promiscuous_targets=args.remove_promiscuous_targets, min_ligand_size=args.min_ligand_size,
                                   multiplicity= args.train_multiplicity, unroll_clusters=args.unroll_clusters,
@@ -103,24 +107,24 @@ def construct_loader(args, t_to_sigma, device):
             else:
                 train_dataset = train_dataset2
 
-        if args.dataset == 'pdbbind' or args.double_val:
-            val_dataset = PDBBind(cache_path=args.cache_path, split_path=args.split_val, keep_original=True,
-                                  esm_embeddings_path=args.pdbbind_esm_embeddings_path, root=args.pdbbind_dir,
-                                  protein_file=args.protein_file, require_ligand=True, **common_args)
-            if args.double_val:
-                val_dataset2 = val_dataset
+        # if args.dataset == 'pdbbind' or args.double_val:
+        #     val_dataset = PDBBind(cache_path=args.cache_path, split_path=args.split_val, keep_original=True,
+        #                           esm_embeddings_path=args.pdbbind_esm_embeddings_path, root=args.pdbbind_dir,
+        #                           protein_file=args.protein_file, require_ligand=True, **common_args)
+        #     if args.double_val:
+        #         val_dataset2 = val_dataset
 
-        if args.dataset == 'moad' or args.dataset == 'generalisation':
-            val_dataset = MOAD(cache_path=args.cache_path, split='val', keep_original=True,
-                               multiplicity= args.val_multiplicity, max_receptor_size=args.max_receptor_size,
-                               remove_promiscuous_targets=args.remove_promiscuous_targets, min_ligand_size=args.min_ligand_size,
-                               esm_embeddings_sequences_path=args.moad_esm_embeddings_sequences_path,
-                               unroll_clusters=args.unroll_clusters, root=args.moad_dir,
-                               esm_embeddings_path=args.moad_esm_embeddings_path, require_ligand=True, **common_args)
+        # if args.dataset == 'moad' or args.dataset == 'generalisation':
+        #     val_dataset = MOAD(cache_path=args.cache_path, split='val', keep_original=True,
+        #                        multiplicity= args.val_multiplicity, max_receptor_size=args.max_receptor_size,
+        #                        remove_promiscuous_targets=args.remove_promiscuous_targets, min_ligand_size=args.min_ligand_size,
+        #                        esm_embeddings_sequences_path=args.moad_esm_embeddings_sequences_path,
+        #                        unroll_clusters=args.unroll_clusters, root=args.moad_dir,
+        #                        esm_embeddings_path=args.moad_esm_embeddings_path, require_ligand=True, **common_args)
 
         loader_class = DataListLoader if torch.cuda.is_available() else DataLoader
 
-    train_loader = loader_class(dataset=train_dataset, batch_size=args.batch_size, num_workers=args.num_dataloader_workers, shuffle=True, pin_memory=args.pin_memory, drop_last=args.dataloader_drop_last)
-    val_loader = loader_class(dataset=val_dataset, batch_size=args.batch_size, num_workers=args.num_dataloader_workers, shuffle=False, pin_memory=args.pin_memory, drop_last=args.dataloader_drop_last)
-    return train_loader, val_loader, val_dataset2
+    test_loader = loader_class(dataset=train_dataset, batch_size=args.batch_size, num_workers=args.num_dataloader_workers, shuffle=True, pin_memory=args.pin_memory, drop_last=args.dataloader_drop_last)
+    # val_loader = loader_class(dataset=val_dataset, batch_size=args.batch_size, num_workers=args.num_dataloader_workers, shuffle=False, pin_memory=args.pin_memory, drop_last=args.dataloader_drop_last)
+    return test_loader
 
